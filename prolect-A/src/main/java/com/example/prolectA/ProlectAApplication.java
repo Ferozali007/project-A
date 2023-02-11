63a4ac869c807e0e0321e58b8203db2c6068cc9b
LENOVO <LENOVO@FEROZ>
2023-02-11 19:06:53 +0530

    secondtry

---
+++ prolect-A/src/main/java/com/example/prolectA/ProlectAApplication.java
@@ -0,0 +1,13 @@
+package com.example.prolectA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProlectAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProlectAApplication.class, args);
+	}
+
+}
---
+++ prolect-A/src/test/java/com/example/prolectA/ProlectAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prolectA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProlectAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
